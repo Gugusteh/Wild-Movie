7483edf258c2ab93a68ed3951d76208380daba81
Gugusteh <augustin.lefebvre83@gmail.com>
2016-07-19 11:36:35 +0200

    fini

--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -5,6 +5,10 @@ namespace FrontendBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FrontendBundle\Entity\Film;
 use Symfony\Component\HttpFoundation\Request;
+use FrontendBundle\Entity\Comment;
+use AppBundle\Entity\User;
+use DateTime;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
@@ -60,13 +64,61 @@ class DefaultController extends Controller
     {
     	$em = $this->getDoctrine()->getManager();
         $user = $this->container->get('security.context')->getToken()->getUser();
-
+        $comments = $em->getRepository('FrontendBundle:Comment')->findByIdfilm($id);
+        if (!empty($comments))
+         {
+            $commenttab = [];
+            foreach ($comments as $comment) 
+            { 
+                $username=$em->getRepository('AppBundle:User')->findOneById($comment->getIduser())->getUsername();
+                $commenttab[] = array(
+                    'idfilm'=>$id,
+                    'username'=>$username,
+                    'date'=>$comment->getDate(),
+                    'content'=>$comment->getContent(),
+                    );
+            }
+        }
+        else
+        {
+            $commenttab = 0;
+        }
         $thisMovie = $em->getRepository('FrontendBundle:Film')->findOneById($id);
 
-        return $this->render('FrontendBundle:Default:show.html.twig',
-        	array(
+        return $this->render('FrontendBundle:Default:show.html.twig', array(
         		'movie' => $thisMovie,
+                'comments'=>$commenttab,
+                'id'=>$id,
         ));
 
+    }
+    public function newCommentAction(Request $request, $id)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $user=$this->getUser();
+        $content = $request->request->get('content');
+        if(!empty($content) && !empty($user))
+        {
+            $comment = new Comment();
+            $comment->setIduser($user->getId());
+            $comment->setIdfilm($id);
+            $comment->setDate(new Datetime);
+            $comment->setContent($content);
+            $em->persist($comment);
+            $em->flush();
+
+        }
+        elseif (empty($content))
+        {
+            echo "remplis le formulaire avant de valider";
+        }
+        elseif (empty($user))
+        {
+            echo "connecte toi";
+        }
+
+        $url = $this -> generateUrl('film_show', array( 'id'=>$id ));
+        $response = new RedirectResponse($url);
+        return $response;
     }
 }
---
+++ src/FrontendBundle/Entity/Comment.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace FrontendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comment
+ *
+ * @ORM\Table(name="comment")
+ * @ORM\Entity(repositoryClass="FrontendBundle\Repository\CommentRepository")
+ */
+class Comment
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="iduser", type="integer")
+     */
+    private $iduser;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idfilm", type="integer")
+     */
+    private $idfilm;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text", nullable=true)
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set iduser
+     *
+     * @param integer $iduser
+     * @return Comment
+     */
+    public function setIduser($iduser)
+    {
+        $this->iduser = $iduser;
+
+        return $this;
+    }
+
+    /**
+     * Get iduser
+     *
+     * @return integer 
+     */
+    public function getIduser()
+    {
+        return $this->iduser;
+    }
+
+    /**
+     * Set idfilm
+     *
+     * @param integer $idfilm
+     * @return Comment
+     */
+    public function setIdfilm($idfilm)
+    {
+        $this->idfilm = $idfilm;
+
+        return $this;
+    }
+
+    /**
+     * Get idfilm
+     *
+     * @return integer 
+     */
+    public function getIdfilm()
+    {
+        return $this->idfilm;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Comment
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Comment
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
